6c0c047cacd24ea64af37bf8e692dceca510819e
Jennie Lust <94746740+JennieLust@users.noreply.github.com>
2022-09-17 15:12:15 +0200

    slight code cleanup

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 /*              GOAL: as little global code as possible         */
 
 /* TODO 
-    Fix: names remain in form when reloading 
+    Fix: names remain in form when reloading
+    When starting game, enter default names and show displays
     */
+
+
 const body = document.querySelector("body")
 const gameContainer = document.querySelector(".game-container")
 const cell0 = document.querySelector(".cell-0")
@@ -27,74 +30,58 @@ var p1Turn = document.createElement("p");
 
 var resDisplay = document.querySelector(".results")
 
-
 var p1Icon = ""
 var p2Icon = ""
 
-/* initializes the game via the start/spela button */
-startButton.addEventListener('click', () => {
+startButton.addEventListener('click', () => {               /* initializes the game via the start/spela button */
     if (startButton.innerText === "SPELA") {
-        /* pop up info-sidebars */
-        p1Info.style.visibility = "visible";
+
+        p1Info.style.visibility = "visible";                /* pop up info-sidebars */
         p2Info.style.visibility = "visible";
 
         p2Button.addEventListener('click', () => {
             var p2Name = document.querySelector('.p2-name').value;
-            /* set default name*/
-            if (p2Name.length < 1) {
-                p2Name = "Player 2"
-            };
+ 
+            if (p2Name.length < 1) {p2Name = "Player 2"};                /* set default name*/
             const p2 = playerFactory(p2Name, "o");
 
-            /* hides pop up form */
-            document.querySelector(".p2-form").style.display = "none";
-
-            /* creates html display elements */
-            var p2NameDis = document.createElement("p");
+            document.querySelector(".p2-form").style.display = "none";      /* hides pop up form */
+            
+            var p2NameDis = document.createElement("p");        /* creates html display elements */
             p2NameDis.innerText = p2.name;
             p2Info.appendChild(p2NameDis);
             var p2IconDis = document.createElement("p");
             p2IconDis.innerText= "spelar med " + p2.icon;
             p2Info.appendChild(p2IconDis);
-
-            /* creates turn signal which can be hidden/revealed */
-            p2Turn.innerText = `${p2.name}'s turn!`;
+  
+            p2Turn.innerText = `${p2.name}'s turn!`;             /* creates turn signal which can be hidden/revealed */
             p2Turn.style.fontWeight = "bold";
             p2Turn.style.visibility = "hidden";
             p2Info.appendChild(p2Turn);
+        });
 
-
-        })
         p1Button.addEventListener('click', () => {
             var p1Name = document.querySelector('.p1-name').value;
 
-            /* set default name*/
-            if (p1Name.length < 1) {
-                p1Name = "Player 1"
-            };
+            if (p1Name.length < 1) {p1Name = "Player 1"};                   /* set default name*/
+                
             const p1 = playerFactory(p1Name, "x");
+            
+            document.querySelector(".p1-form").style.display = "none";      /* hides pop up form */
 
-            /* hides pop up form */
-            document.querySelector(".p1-form").style.display = "none";
-
-            /* creates html display elements */
-            var p1NameDis = document.createElement("p");
+            var p1NameDis = document.createElement("p");        /* creates html display elements */
             p1NameDis.innerText = p1.name;
             p1Info.appendChild(p1NameDis);
             var p1IconDis = document.createElement("p");
             p1IconDis.innerText= "spelar med " + p1.icon;
             p1Info.appendChild(p1IconDis);
 
-             /* creates turn signal which can be hidden/revealed */
-            p1Turn.innerText = `${p1.name}'s turn!`;
+            p1Turn.innerText = `${p1.name}'s turn!`;            /* creates turn signal which can be hidden/revealed */
             p1Turn.style.fontWeight = "bold";
             p1Turn.style.visibility = "visible";
             p1Info.appendChild(p1Turn);
-
-
-        })
+        });
         startButton.innerText = "BÖRJA OM"
-
     } else if (startButton.innerText === "BÖRJA OM") {
         gameBoard.clear();
         gameBoard.display();
@@ -104,21 +91,15 @@ startButton.addEventListener('click', () => {
         resDisplay.innerText = "";
         resDisplay.style.visibility = "hidden";
         window.location.reload();
-    }   
+    };   
     game();
 });
 
-/* store gameboard as array inside of a gameboard object
-    maybe store inside three inner arrays? is that even needed? could compare against each other easier maybe
-        0   1   2
-        3   4   5
-        6   7   8       */
-const gameBoard = (() => {
-    const arr = ["", "", "", "", "", "", "", "", ""];
-    const add = (icon, position) => arr[position] = icon /* replace array index with icon */
-    const clear = () => {for (let i = 0; i < arr.length; i++) { /* clears array */
+const gameBoard = (() => {      
+    const arr = ["", "", "", "", "", "", "", "", ""];               /* store gameboard as array inside of a gameboard object */
+    const add = (icon, position) => arr[position] = icon            /* replace array index with icon */
+    const clear = () => {for (let i = 0; i < arr.length; i++) {     /* clears array */
         arr[i] = "" }} 
-    /* display function */
     const display = () => {
         cell0.innerText = arr[0];
         cell1.innerText = arr[1];
@@ -138,32 +119,22 @@ const gameBoard = (() => {
     };
 })();
 
-
 const game = function() {
-    let gameTurn = 1;
-    
-    /* decides which icon/turn it is */
-    let iconify = function () {
+    let gameTurn = 1;                   /* keeps track of turn order via odd/even */
+    let iconify = function () {         /* decides which icon/turn it is */
         let currentIcon = ""
-        if (gameTurn % 2 != 0) {
-            currentIcon = "x"
-        } else {
-            currentIcon = "o"
-    }
+        if (gameTurn % 2 != 0) {currentIcon = "x"
+        } else {currentIcon = "o"}
     return currentIcon;
-    }
+    };
 
-    /* move function */
-    let move = function (cell, index) {
+    let move = function (cell, index) {         /* move function */
         if (cell.innerText === "") {
             cell.innerText = iconify();
             gameBoard.add(iconify(), index);
-            /* determines if check-for-winner function should be called */
-            if (gameTurn > 3) {
-                checkWinner(iconify());
-            } 
+            if (gameTurn > 3) {checkWinner(iconify())};                   /* determines if check-for-winner function should be called */
             gameTurn = gameTurn + 1;
-        }
+        };
         if (gameTurn % 2 != 0) {
             p2Turn.style.visibility = "hidden";
             p1Turn.style.visibility = "visible";
@@ -171,10 +142,10 @@ const game = function() {
             p2Turn.style.visibility = "visible";
             p1Turn.style.visibility = "hidden";
         }
-    }
+    };
 
-    /* function that checks for winning combinations */
-    let checkWinner = function (icon) {
+    
+    let checkWinner = function (icon) {             /* function that checks for winning combinations */
         /* function to shorten code - tests gameboard condition against patterns */
         let checkPattern = function (icon, index1, index2, index3) {
             if (gameBoard.arr[index1] === icon && gameBoard.arr[index2] === icon && gameBoard.arr[index3] === icon) {
@@ -194,40 +165,22 @@ const game = function() {
         checkPattern(icon, 2, 4, 6);
     }
 
-    /* event listeners */
-    cell0.addEventListener('click', () => {
-        move(cell0, 0)
-    });
-    cell1.addEventListener('click', () => {
-        move(cell1, 1)
-    });
-    cell2.addEventListener('click', () => {
-        move(cell2, 2)
-    });
-    cell3.addEventListener('click', () => {
-        move(cell3, 3)
-    });
-    cell4.addEventListener('click', () => {
-        move(cell4, 4)
-    });
-    cell5.addEventListener('click', () => {
-        move(cell5, 5)
-    });
-    cell6.addEventListener('click', () => {
-        move(cell6, 6)
-    });
-    cell7.addEventListener('click', () => {
-        move(cell7, 7)
-    });
-    cell8.addEventListener('click', () => {
-        move(cell8, 8)
-    });
-}
+    /* event listeners call move function with cell & index to be filled */
+    cell0.addEventListener('click', () => {move(cell0, 0)});
+    cell1.addEventListener('click', () => {move(cell1, 1)});
+    cell2.addEventListener('click', () => {move(cell2, 2)});
+    cell3.addEventListener('click', () => {move(cell3, 3)});
+    cell4.addEventListener('click', () => {move(cell4, 4)});
+    cell5.addEventListener('click', () => {move(cell5, 5)});
+    cell6.addEventListener('click', () => {move(cell6, 6)});
+    cell7.addEventListener('click', () => {move(cell7, 7)});
+    cell8.addEventListener('click', () => { move(cell8, 8)});
+};
 
-gameBoard.display();
+gameBoard.display();    /* makes sure the gameboard is visible at the start */
 
-/* players stored in objects / factory */
-const playerFactory = (name, icon) => {
+
+const playerFactory = (name, icon) => { /* players stored in objects / factory */
     return { name, icon };
 }
 
