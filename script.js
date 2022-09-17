4a310c74d76032fa9ad641311a6dd34bbe4be3c8
Jennie Lust <94746740+JennieLust@users.noreply.github.com>
2022-09-17 15:00:44 +0200

    full functionality
    minor changes needed in the future as well as a re-writing of the code for better readability and resource use

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 /*              GOAL: as little global code as possible         */
 
 /* TODO 
-    BUG: 
-        Player Turn Display only shows up when pressing redo
-        On redo/restart, names remain entered - clear them
-        Börja om does not clear gameboard 
-
-
-    Object for game flow
-        - done - if x - player starts
-        display on screen 
-        - done - when div pressed - innerhtml their icon (where do I add eventlistener?)
-        - done - next players turn (keep track w i++ odd or even?) (done, but needs functional incrementing)
-        when three turns - start determining if game fullfills winning conditiion
-        when no empty squares remain / after 9 turns an no win condition fullfillment -> draw
-        determine who won if win condition fullfilled 
-    display results
+    Fix: names remain in form when reloading 
     */
-
+const body = document.querySelector("body")
 const gameContainer = document.querySelector(".game-container")
 const cell0 = document.querySelector(".cell-0")
 const cell1 = document.querySelector(".cell-1")
@@ -39,10 +25,13 @@ const p2Button = document.querySelector(".p2-button")
 var p2Turn = document.createElement("p");
 var p1Turn = document.createElement("p");
 
+var resDisplay = document.querySelector(".results")
+
 
 var p1Icon = ""
 var p2Icon = ""
-/* change so players are created as objects*/
+
+/* initializes the game via the start/spela button */
 startButton.addEventListener('click', () => {
     if (startButton.innerText === "SPELA") {
         /* pop up info-sidebars */
@@ -69,7 +58,6 @@ startButton.addEventListener('click', () => {
             p2Info.appendChild(p2IconDis);
 
             /* creates turn signal which can be hidden/revealed */
-            
             p2Turn.innerText = `${p2.name}'s turn!`;
             p2Turn.style.fontWeight = "bold";
             p2Turn.style.visibility = "hidden";
@@ -98,28 +86,25 @@ startButton.addEventListener('click', () => {
             p1Info.appendChild(p1IconDis);
 
              /* creates turn signal which can be hidden/revealed */
-            
             p1Turn.innerText = `${p1.name}'s turn!`;
             p1Turn.style.fontWeight = "bold";
-            p1Turn.style.visibility = "hidden";
+            p1Turn.style.visibility = "visible";
             p1Info.appendChild(p1Turn);
 
 
         })
         startButton.innerText = "BÖRJA OM"
 
-        /* if both names added */
-        
-
-        /* if none names added but a click has been made, still go through with calling game*/
-
     } else if (startButton.innerText === "BÖRJA OM") {
         gameBoard.clear();
+        gameBoard.display();
         startButton.innerHTML = "SPELA"
         p1Info.style.visibility = "hidden";
         p2Info.style.visibility = "hidden";
+        resDisplay.innerText = "";
+        resDisplay.style.visibility = "hidden";
+        window.location.reload();
     }   
-
     game();
 });
 
@@ -157,18 +142,13 @@ const gameBoard = (() => {
 const game = function() {
     let gameTurn = 1;
     
-
     /* decides which icon/turn it is */
     let iconify = function () {
         let currentIcon = ""
         if (gameTurn % 2 != 0) {
             currentIcon = "x"
-            p2Turn.style.visibility = "hidden";
-            p1Turn.style.visibility = "visible";
         } else {
             currentIcon = "o"
-            p2Turn.style.visibility = "visible";
-            p1Turn.style.visibility = "hidden";
     }
     return currentIcon;
     }
@@ -178,16 +158,19 @@ const game = function() {
         if (cell.innerText === "") {
             cell.innerText = iconify();
             gameBoard.add(iconify(), index);
+            /* determines if check-for-winner function should be called */
             if (gameTurn > 3) {
                 checkWinner(iconify());
-            }
+            } 
             gameTurn = gameTurn + 1;
-  
-        } else {
-            alert("You cannot choose an occupied square. Please choose another move.")
         }
-        /* determines if check-for-winner function should be called */
-
+        if (gameTurn % 2 != 0) {
+            p2Turn.style.visibility = "hidden";
+            p1Turn.style.visibility = "visible";
+        } else {
+            p2Turn.style.visibility = "visible";
+            p1Turn.style.visibility = "hidden";
+        }
     }
 
     /* function that checks for winning combinations */
@@ -195,8 +178,11 @@ const game = function() {
         /* function to shorten code - tests gameboard condition against patterns */
         let checkPattern = function (icon, index1, index2, index3) {
             if (gameBoard.arr[index1] === icon && gameBoard.arr[index2] === icon && gameBoard.arr[index3] === icon) {
-                /* Do winner decleration */
-                console.log("yeah")
+                resDisplay.innerText =  icon + " vinner!";
+                p1Turn.style.visibility = "hidden";
+                p2Turn.style.visibility = "hidden";
+                resDisplay.style.visibility = "visible";
+                body.appendChild(resDisplay);
             }};
         checkPattern(icon, 0, 3, 6);
         checkPattern(icon, 2, 4, 7);
@@ -207,7 +193,6 @@ const game = function() {
         checkPattern(icon, 0, 4, 8);
         checkPattern(icon, 2, 4, 6);
     }
-
 
     /* event listeners */
     cell0.addEventListener('click', () => {
@@ -237,92 +222,15 @@ const game = function() {
     cell8.addEventListener('click', () => {
         move(cell8, 8)
     });
-
-
-
-
-
 }
 
-
-
-
-
-
-
-
-
 gameBoard.display();
-
-
-
-
-
-
 
 /* players stored in objects / factory */
 const playerFactory = (name, icon) => {
     return { name, icon };
 }
 
-/* how would an object control the flow of the game? 
-
-/* object to control the flow of the game itself 
-    DONE a. enter player names - default to player 1 and 2
-    DONE b. assign icon 
-    1. first move to x? 
-
-    let moveCount = 1; 
-    if (moveCount %)
-    2. display whose move it is and their icon
-    3. on click on cell (grid item) enter current players icon - disable click on cell already filled
-    4. repeat for other player (how do i determine player, var with number and if(odd or even))
-    5. when win condition is fullfilled or no empty strings remain - determine winner and display
-*/
-
-/* set up your HTML and write JS to render to webpage 
-    - ALWAYS: 
-        -title
-        -gameboard 
-    
-    -BEFORE GAME: 
-        -button - start 
-
-    -START BUTTON PRESSED: 
-        -fill in player names
-
-    -DURING GAME: 
-        -button - restart (not fully functional)
-        display turn and -icon
-    
-    AFTER GAME: 
-        button - restart 
-        game results
-
-*/
-
-/* logic that determines end of game */
-/* Manual or is it possible automatic w maths?
-
-
-check for tie [if all cells filled but none fullfills win conditions - draw]
-   
-if ($LOGIC THAT DETERMINES WINNER) {
-    displayRes("Spelare $PLAYERNAME vann! Gratulationer, jag visste att det skulle vara du! Jag är så stolt!")" 
-} else {
-    displayRes("Ingen vann, ingen var värdig...")
-}
-
-function to display results
-function displayRes(string) {
-}
-*/
-
-
-/* clean up interface: 
-allow players to put in their name
-button to restart/start the game
-display element that congratulates winning player */
 
 /* long term goal: 
     create an AI so player vs pc is possible
